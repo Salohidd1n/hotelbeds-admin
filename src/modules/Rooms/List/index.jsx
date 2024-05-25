53572bbaf9b81c480b4fb42141fcf94635e04ef4
salohiddin <saloh224@gmail.com>
2024-05-26 00:27:06 +0500

    fix delete button

--- src/modules/Rooms/List/index.jsx
+++ src/modules/Rooms/List/index.jsx
@@ -1,4 +1,4 @@
-import { AddIcon, DeleteIcon, DownloadIcon } from '@chakra-ui/icons';
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons';
 import { Box, Button, IconButton } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -16,12 +16,14 @@ import styles from './index.module.scss';
 import { useGetRooms, useRoomsDelete } from 'services/room.service';
 import SearchInput from 'components/FormElements/Input/SearchInput';
 import useDebounce from 'hooks/useDebounce';
+import CustomPopup from 'components/CustomPopup';
 
 const RoomListPage = () => {
 	const navigate = useNavigate();
 	const { pathname } = useLocation();
 	const [pageSize, setPageSize] = useState(30);
 	const [page, setPage] = useState(1);
+	const [deletableRoom, setDeletableRoom] = useState(false);
 	const [term, setTerm] = useState();
 	const {
 		data: { hits, count } = {},
@@ -40,7 +42,10 @@ const RoomListPage = () => {
 	}, 700);
 
 	const { mutate: deleteUser, isLoading: deleteLoading } = useRoomsDelete({
-		onSuccess: refetch,
+		onSuccess: () => {
+			refetch();
+			setDeletableRoom(null);
+		},
 	});
 
 	const navigateToCreatePage = () => {
@@ -86,7 +91,10 @@ const RoomListPage = () => {
 			align: 'center',
 			render: (_, row, index) => (
 				<IconButton
-					onClick={(e) => onDeleteClick(e, row)}
+					onClick={(e) => {
+						e.stopPropagation();
+						setDeletableRoom(row);
+					}}
 					colorScheme="red"
 					variant="outline"
 				>
@@ -96,57 +104,89 @@ const RoomListPage = () => {
 		},
 	];
 
+	console.log('deletableRoom', deletableRoom);
+
 	return (
-		<Box>
-			<Header>
-				<HeaderLeftSide>
-					<HeaderTitle>Rooms</HeaderTitle>
-				</HeaderLeftSide>
-				<HeaderExtraSide>
-					<NotificationMenu />
-					<ProfileMenu />
-				</HeaderExtraSide>
-			</Header>
+		<>
+			<Box>
+				<Header>
+					<HeaderLeftSide>
+						<HeaderTitle>Rooms</HeaderTitle>
+					</HeaderLeftSide>
+					<HeaderExtraSide>
+						<NotificationMenu />
+						<ProfileMenu />
+					</HeaderExtraSide>
+				</Header>
 
-			<Page p={4}>
-				<PageCard h="calc(100vh - 90px)">
-					<PageCardHeader>
-						<HeaderExtraSide>
-							<Box w="250px">
-								<SearchInput onChange={onChangeTerm} />
-							</Box>
-							<Button
-								onClick={navigateToCreatePage}
-								bgColor="primary.main"
-								leftIcon={<AddIcon />}
-							>
-                Create room
-							</Button>
-						</HeaderExtraSide>
-					</PageCardHeader>
+				<Page p={4}>
+					<PageCard h="calc(100vh - 90px)">
+						<PageCardHeader>
+							<HeaderExtraSide>
+								<Box w="250px">
+									<SearchInput onChange={onChangeTerm} />
+								</Box>
+								<Button
+									onClick={navigateToCreatePage}
+									bgColor="primary.main"
+									leftIcon={<AddIcon />}
+								>
+                  Create room
+								</Button>
+							</HeaderExtraSide>
+						</PageCardHeader>
 
-					<Box p={3}>
-						<DataTable
-							columns={columns}
-							data={hits}
-							scroll={{ y: 'calc(100vh - 260px)' }}
-							isLoading={isLoading || deleteLoading}
-							pagination={{
-								total: Number(count || 0),
-								pageSize,
-								onPageSizeChange: setPageSize,
-								onChange: onChangePage,
-								current: page,
+						<Box p={3}>
+							<DataTable
+								columns={columns}
+								data={hits}
+								scroll={{ y: 'calc(100vh - 260px)' }}
+								isLoading={isLoading}
+								pagination={{
+									total: Number(count || 0),
+									pageSize,
+									onPageSizeChange: setPageSize,
+									onChange: onChangePage,
+									current: page,
+								}}
+								onRow={(row, index) => ({
+									onClick: () => navigateToEditPage(row.id),
+								})}
+								className={styles.table}
+							/>
+						</Box>
+					</PageCard>
+				</Page>
+			</Box>
+			<CustomPopup
+				isOpen={!!deletableRoom}
+				title="Delete Room"
+				footerContent={
+					<Box display="flex" gap="3">
+						<Button variant="outline" onClick={() => setDeletableRoom(null)}>
+              Cancel
+						</Button>
+						<Button
+							variant="solid"
+							bg="red"
+							disabled={deleteLoading}
+							isLoading={deleteLoading}
+							onClick={(e) => onDeleteClick(e, deletableRoom)}
+							_hover={{
+								background: 'red',
 							}}
-							onRow={(row, index) => ({
-								onClick: () => navigateToEditPage(row.id),
-							})}
-							className={styles.table}
-						/>
+						>
+              Delete
+						</Button>
 					</Box>
-				</PageCard>
-			</Page>
-		</Box>
+				}
+				onClose={() => setDeletableRoom(null)}
+			>
+				<p>
+          Are you sure want to delete <b>{deletableRoom?.en_name}</b> room?
+				</p>
+			</CustomPopup>
+		</>
 	);
 };
 export default RoomListPage;
