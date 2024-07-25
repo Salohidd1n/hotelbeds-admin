1c3f488683c3aa64b1c6f1eb2f09f7b4d43cae0f
Otabek <otabektuit11@gmail.com>
2024-07-25 18:01:31 +0500

    change: changed signature date

--- src/utils/generateSignature.js
+++ src/utils/generateSignature.js
@@ -1,7 +1,7 @@
 import CryptoJS from 'crypto-js';
 
 export default function generateSignature() {
-	const timestamp = Math.floor(Date.now() / 1000);
+	const timestamp = Math.floor(Date.now() / 3000);
 	return {
 		key: CryptoJS.HmacSHA256(
 			`${import.meta.env.VITE_API_KEY}${timestamp}`,
